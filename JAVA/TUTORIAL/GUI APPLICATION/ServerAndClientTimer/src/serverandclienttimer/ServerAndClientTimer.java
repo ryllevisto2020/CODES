6165f14812a51c1467e25a81efcbea237390a8c0
ryllevisto2020 <ryllevisto13@gmail.com>
2024-02-07 11:04:50 +0800

    LOAD

---
+++ JAVA/TUTORIAL/GUI APPLICATION/ServerAndClientTimer/src/serverandclienttimer/ServerAndClientTimer.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package serverandclienttimer;
+
+import java.awt.Window;
+
+/**
+ *
+ * @author rylle
+ */
+public class ServerAndClientTimer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+}
